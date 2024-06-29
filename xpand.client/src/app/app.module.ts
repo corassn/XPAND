644fa0f09ad60db442c67103f330f4b95833bcca
Cora Sasaran <corasasaran@gmail.com>
2024-06-29 23:32:24 +0300

    Fix header

--- xpand.client/src/app/app.module.ts
+++ xpand.client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanetsModule } from './planets/planets.module';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
