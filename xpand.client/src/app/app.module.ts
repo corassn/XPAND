3cadab0d80f778af34ab8a81bcf96618e2c6dd21
Cora Sasaran <corasasaran@gmail.com>
2024-07-02 12:32:46 +0300

    Add user menu with 'add' and 'logout' options

--- xpand.client/src/app/app.module.ts
+++ xpand.client/src/app/app.module.ts
@@ -1,26 +1,36 @@
 import { provideHttpClient } from '@angular/common/http';
 import { NgModule, isDevMode } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlanetsModule } from './planets/planets.module';
 import { HeaderComponent } from './header/header.component';
 import { CoreModule } from './core/core.module';
+import { UserMenuComponent } from './user-menu/user-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    UserMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     PlanetsModule,
     CoreModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    BrowserAnimationsModule,
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
@@ -30,4 +40,9 @@ import { CoreModule } from './core/core.module';
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+    this.matIconRegistry.addSvgIcon(`chevron-down`, this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/images/chevron-down.svg'));
+    this.matIconRegistry.addSvgIcon(`more-vert`, this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/images/more-vert.svg'));
+  }
+}
